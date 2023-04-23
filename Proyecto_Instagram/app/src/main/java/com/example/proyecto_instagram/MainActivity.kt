c9a5062990d2f6a0caee488b11b9fb77843b0cba
Michel <codigoronin@gmail.com>
2023-04-23 18:06:07 +0200

    Todo con Dagger Hilt

--- Proyecto_Instagram/app/src/main/java/com/example/proyecto_instagram/MainActivity.kt
+++ Proyecto_Instagram/app/src/main/java/com/example/proyecto_instagram/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.proyecto_instagram
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -12,8 +13,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.proyecto_instagram.login.ui.LoginViewModel
 import com.example.proyecto_instagram.login.ui.LoginScreen
 import com.example.proyecto_instagram.ui.theme.Proyecto_InstagramTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+// La idea de la inyeccion de dependencias es que no haya ninguna instancia fuera de la
+// inyeccion de dependencias ( ViewModels, caso de uso, ...)
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val loginViewModel: LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                   LoginScreen(loginViewModel= LoginViewModel())
+                   LoginScreen(loginViewModel= loginViewModel)
                 }
             }
         }
---
+++ Proyecto_Instagram/app/src/main/java/com/example/proyecto_instagram/core/InstagramApp.kt
@@ -0,0 +1,8 @@
+package com.example.proyecto_instagram.core
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class InstagramApp : Application() {
+}
