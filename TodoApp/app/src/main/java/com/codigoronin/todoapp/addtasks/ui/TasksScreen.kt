06bdd92c326c79cdbc8b1b4e58de9500064c6131
Michel <codigoronin@gmail.com>
2023-04-29 18:34:16 +0200

    Add first part

--- TodoApp/app/src/main/java/com/codigoronin/todoapp/addtasks/ui/TasksScreen.kt
+++ TodoApp/app/src/main/java/com/codigoronin/todoapp/addtasks/ui/TasksScreen.kt
@@ -1,39 +1,87 @@
 package com.codigoronin.todoapp.addtasks.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import com.codigoronin.todoapp.addtasks.ui.model.TaskModel
 
 @Composable
 
 fun TasksScreen(tasksViewModel: TasksViewModel) {
 
+    val showDialog: Boolean by tasksViewModel.showDialog.observeAsState(initial = false)
+
     Box(modifier = Modifier.fillMaxSize()) {
-        AddTaskDialog(show = true, onDismiss = {}, onTaskAdded = {})
-        FabDialog(Modifier.align(Alignment.BottomEnd))
+        AddTaskDialog(
+            show = showDialog,
+            onDismiss = { tasksViewModel.onDialogClose() },
+            onTaskAdded = { tasksViewModel.onTaskAdd(it) })
+        FabDialog(Modifier.align(Alignment.BottomEnd), tasksViewModel)
+        TaskList(tasksViewModel)
     }
 
 }
 
 @Composable
-fun FabDialog(modifier: Modifier) {
+fun TaskList(tasksViewModel: TasksViewModel) {
+    val myTasks: List<TaskModel> = tasksViewModel.tasks
+    LazyColumn() {
+        items(items = myTasks, key = { it.id }) {
+            ItemTask(taskModel = it, tasksViewModel = tasksViewModel)
+        }
+    }
+}
+
+@Composable
+fun ItemTask(taskModel: TaskModel, tasksViewModel: TasksViewModel) {
+    Card(
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .pointerInput(Unit) {
+                detectTapGestures(onLongPress = { tasksViewModel.onItemRemove(taskModel) })
+            },
+        elevation = 4.dp
+    ) {
+        Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = taskModel.task,
+                Modifier
+                    .weight(1f)
+                    .padding(horizontal = 16.dp)
+            )
+            Checkbox(
+                checked = taskModel.selected,
+                onCheckedChange = { tasksViewModel.onCheckBoxSelected(taskModel) })
+        }
+
+    }
+}
+
+@Composable
+fun FabDialog(modifier: Modifier, tasksViewModel: TasksViewModel) {
 
     FloatingActionButton(
-        onClick = { },
+        onClick = { tasksViewModel.onShowDialogClick() },
         modifier = modifier
             .padding(16.dp), backgroundColor = Color.Cyan
     ) {
@@ -42,8 +90,9 @@ fun FabDialog(modifier: Modifier) {
 }
 
 @Composable
-fun AddTaskDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded:(String) -> Unit) {
+fun AddTaskDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded: (String) -> Unit) {
     var myTask by rememberSaveable { mutableStateOf("") }
+
     if (show) {
         Dialog(onDismissRequest = { onDismiss() }) {
 
@@ -68,7 +117,10 @@ fun AddTaskDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded:(String) -> 
                     maxLines = 1
                 )
                 Spacer(modifier = Modifier.size(16.dp))
-                Button(onClick = { onTaskAdded(myTask) }, Modifier.fillMaxWidth()) {
+                Button(onClick = {
+                    onTaskAdded(myTask)
+                    myTask = ""
+                }, Modifier.fillMaxWidth()) {
                     Text(text = "Add Task")
                 }
             }
--- TodoApp/app/src/main/java/com/codigoronin/todoapp/addtasks/ui/TasksViewModel.kt
+++ TodoApp/app/src/main/java/com/codigoronin/todoapp/addtasks/ui/TasksViewModel.kt
@@ -1,7 +1,50 @@
 package com.codigoronin.todoapp.addtasks.ui
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.codigoronin.todoapp.addtasks.ui.model.TaskModel
 import javax.inject.Inject
 
-class TasksViewModel @Inject constructor(): ViewModel() {
+class TasksViewModel @Inject constructor() : ViewModel() {
+
+
+    private val _showDialog = MutableLiveData<Boolean>()
+    val showDialog: LiveData<Boolean> = _showDialog
+
+    // Como los LiveData no funcionan del bien con los listados y mas los que se tiene que actulizar
+    // Se podria usar Flows o un mutablestatelist
+
+    private val _tasks = mutableStateListOf<TaskModel>()
+    val tasks: List<TaskModel> = _tasks
+
+    fun onDialogClose() {
+        _showDialog.value = false
+    }
+
+    fun onTaskAdd(task: String) {
+        _tasks.add(TaskModel(task = task))
+        _showDialog.value = false
+    }
+
+    fun onShowDialogClick() {
+        _showDialog.value = true
+    }
+
+    fun onCheckBoxSelected(taskModel: TaskModel) {
+        val index = _tasks.indexOf(taskModel)
+        //  _tasks[index].selected = !_tasks[index].selected // Deberia ser asi pero no sabe que ha cambiado por lo que no lo redibuja
+
+        _tasks[index] =
+            _tasks[index].let {// Para solucionarlo lo copiamos entero con el valor selected cambiado
+                it.copy(selected = !it.selected)
+            }
+    }
+
+    fun onItemRemove(taskModel: TaskModel) {
+        val task = _tasks.find { it.id == taskModel.id }
+        _tasks.remove(task)
+    }
+
 }
